2706d7c939471a603e7bcdd9fcca514cccfdcfd2
ricardo-temple <ricardovtemple@gmail.com>
2024-05-31 15:49:11 -0300

    add POST method to PhotoRecordController (createPhotoRecord)

--- src/main/java/com/example/SouthernOceanSentinel_API/controller/PhotoRecordController.java
+++ src/main/java/com/example/SouthernOceanSentinel_API/controller/PhotoRecordController.java
@@ -5,10 +5,11 @@ import com.example.SouthernOceanSentinel_API.model.PhotoRecord;
 import com.example.SouthernOceanSentinel_API.service.PhotoRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/records")
@@ -16,5 +17,21 @@ public class PhotoRecordController {
     @Autowired
     private PhotoRecordService photoRecordService;
 
+    @PostMapping("{locationId}")
+    public ResponseEntity<PhotoRecord> createPhotoRecord(
+            @PathVariable Long locationId,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("description") String description,
+            @RequestParam("waterTemp") String waterTemp) {
+
+        try {
+            PhotoRecord photoRecord = photoRecordService.savePhotoRecord(locationId ,file, description, waterTemp);
+            return ResponseEntity.ok(photoRecord);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
 
 }
--- src/main/java/com/example/SouthernOceanSentinel_API/service/PhotoRecordService.java
+++ src/main/java/com/example/SouthernOceanSentinel_API/service/PhotoRecordService.java
@@ -1,12 +1,32 @@
 package com.example.SouthernOceanSentinel_API.service;
-
-import com.example.SouthernOceanSentinel_API.repository.PhotoRecordRepository;
+import com.example.SouthernOceanSentinel_API.model.Location;
+import com.example.SouthernOceanSentinel_API.model.PhotoRecord;
+import com.example.SouthernOceanSentinel_API.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import com.example.SouthernOceanSentinel_API.repository.PhotoRecordRepository;
 
 @Service
 public class PhotoRecordService {
     @Autowired
     private PhotoRecordRepository photoRecordRepository;
 
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public PhotoRecord savePhotoRecord(Long locationId,MultipartFile file, String description, String waterTemp) throws IOException {
+        PhotoRecord photoRecord = new PhotoRecord();
+        photoRecord.setImageData(file.getBytes());
+
+        Location location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid location ID"));
+        photoRecord.setLocation(location);
+        photoRecord.setDescription(description);
+        photoRecord.setWaterTemp(waterTemp);
+
+        return photoRecordRepository.save(photoRecord);
+    }
 }
